29368021b71e4a63da408ee4dc1bdf3cb02f3a72
Kerr <kerr@KerrdeMacBook-Pro.local>
2018-04-08 23:15:24 -0700

    xk

---
+++ Assets/Scripts/CreatePro.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePro : MonoBehaviour {
+	public GameObject gunPrefab;
+	public GameObject bombObjPrefab;
+	public GameObject gumPrefab;
+	public GameObject alienPrefab;
+	// Use this for initialization
+	void Start () {
+		gunPrefab = Resources.Load<GameObject>("Prefabs/gun");
+		bombObjPrefab = Resources.Load<GameObject>("Prefabs/bombobj");
+		gumPrefab = Resources.Load<GameObject>("Prefabs/gum");
+		alienPrefab = Resources.Load<GameObject>("Prefabs/alien");
+
+		Vector2 point1 = new Vector2(Random.Range(-80,-20), Random.Range(11,40));
+		Vector2 point2 = new Vector2(Random.Range(-80,-20), Random.Range(-40,0));
+		Vector2 point21 = new Vector2(Random.Range(-80,-20), Random.Range(-40,0));
+		Vector2 point22 = new Vector2(Random.Range(-80,-20), Random.Range(-40,0));
+		Vector2 point3 = new Vector2(Random.Range(-10,50), Random.Range(30,40));
+		Vector2 point4 = new Vector2(Random.Range(-10,50), Random.Range(-10,0));
+		Vector2 point5 = new Vector2(Random.Range(-80,-20), Random.Range(0,40));
+		Vector2 point6 = new Vector2(Random.Range(-80,-20), Random.Range(-40,0));
+		Vector2 point7 = new Vector2(Random.Range(-10,50), Random.Range(11,20));
+		//gum
+		Vector2 point8 = new Vector2(Random.Range(-80,-20), Random.Range(0,40));
+		Vector2 point9 = new Vector2(Random.Range(39,66), Random.Range(0,40));
+		Vector2 point10 = new Vector2(Random.Range(-10,50), Random.Range(11,20));
+		Vector2 point11 = new Vector2(Random.Range(10,40), Random.Range(11,20));
+		//alien
+		Vector2 point12 = new Vector2(Random.Range(-80,-20), Random.Range(-30,0));
+		Vector2 point13 = new Vector2(Random.Range(5,36), Random.Range(11,20));
+		GameObject gun1 = GameObject.Instantiate(gunPrefab, point1, gunPrefab.transform.rotation) as GameObject;
+		GameObject gun2 = GameObject.Instantiate(gunPrefab, point2, gunPrefab.transform.rotation) as GameObject;
+		GameObject gun3 = GameObject.Instantiate(gunPrefab, point3, gunPrefab.transform.rotation) as GameObject;
+		GameObject gun4 = GameObject.Instantiate(gunPrefab, point4, gunPrefab.transform.rotation) as GameObject;
+		GameObject gun5 = GameObject.Instantiate(gunPrefab, point21, gunPrefab.transform.rotation) as GameObject;
+		GameObject gun6 = GameObject.Instantiate(gunPrefab, point22, gunPrefab.transform.rotation) as GameObject;
+		GameObject bomb1 = GameObject.Instantiate(bombObjPrefab, point5, bombObjPrefab.transform.rotation) as GameObject;
+		GameObject bomb2 = GameObject.Instantiate(bombObjPrefab, point6, bombObjPrefab.transform.rotation) as GameObject;
+		GameObject bomb3 = GameObject.Instantiate(bombObjPrefab, point7, bombObjPrefab.transform.rotation) as GameObject;
+		GameObject gum1 = GameObject.Instantiate(gumPrefab, point8, gumPrefab.transform.rotation) as GameObject;
+		GameObject gum2 = GameObject.Instantiate(gumPrefab, point9, gumPrefab.transform.rotation) as GameObject;
+		GameObject gum3 = GameObject.Instantiate(gumPrefab, point10, gumPrefab.transform.rotation) as GameObject;
+		GameObject gum4 = GameObject.Instantiate(gumPrefab, point11, gumPrefab.transform.rotation) as GameObject;
+		GameObject alien1 = GameObject.Instantiate(alienPrefab, point12, alienPrefab.transform.rotation) as GameObject;
+		GameObject alien2 = GameObject.Instantiate(alienPrefab, point13, alienPrefab.transform.rotation) as GameObject;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/randomlyWalk.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class randomlyWalk : MonoBehaviour {
+
+	public float moveSpeed = 3.0f;
+	public float maxX = -41f;
+	public float minX = -52f;
+	public float maxY = 25f;
+	public float minY = 5f;
+
+	//chicken
+	public float chick_maxX = -46f;
+	public float chick_minX = -65f;
+	public float chick_maxY = 35f;
+	public float chick_minY = 31f;
+
+	private float tChange = 0f; // force new direction in the first Update
+	private float randomX;
+	private float randomY;
+
+	void Update () {
+		// change to random direction at random intervals
+		if (Time.time >= tChange){
+			randomX = Random.Range(-2.0f,2.0f); // with float parameters, a random float
+			randomY = Random.Range(-2.0f,2.0f); //  between -2.0 and 2.0 is returned
+			// set a random interval between 0.5 and 1.5
+			tChange = Time.time + 1.5f;
+		}
+		if (transform.CompareTag ("chick")) {
+			Vector3 v = new Vector3 (randomX, randomY, 0).normalized;
+			transform.Translate(v*moveSpeed* Time.deltaTime);
+			// if object reached any border, revert the appropriate direction
+			if (transform.position.x >= chick_maxX || transform.position.x <= chick_minX) {
+				randomX = -randomX;
+			}
+			if (transform.position.y >= chick_maxY || transform.position.y <= chick_minY) {
+				randomY = -randomY;
+			}
+		}
+		Vector3 v1 = new Vector3 (randomX, randomY, 0).normalized;
+		transform.Translate(v1*moveSpeed* Time.deltaTime);
+		// if object reached any border, revert the appropriate direction
+		if (transform.position.x >= maxX || transform.position.x <= minX) {
+			randomX = -randomX;
+		}
+		if (transform.position.y >= maxY || transform.position.y <= minY) {
+			randomY = -randomY;
+		}
+
+	}
+
+
+
+
+}
