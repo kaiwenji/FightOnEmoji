8b1f626e8c1a145062aa41693220adb8322fe78d
kaiwenji <kaiwenji@usc.edu>
2018-04-15 17:14:17 -0700

    kevin

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+    private float startTime;
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - startTime > 0.5)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
