a230682c3c91df172b5293761c48f0737589600e
kaiwenji <kaiwenji@usc.edu>
2018-04-20 10:31:48 -0700

    update the shrinking circle

--- Assets/Scripts/circleControl.cs
+++ Assets/Scripts/circleControl.cs
@@ -3,47 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.Networking;
 
-public class circleControl : Photon.MonoBehaviour {
+public class circleControl : Photon.MonoBehaviour
+{
     public CircleScript outer;
     public CircleScript inner;
     private float startTime;
     private float shrinkTime;
     private bool isShrinked = false;
     private float xSpeed, ySpeed, speed;
+    private float waitTime = 10f;
     // Use this for initialization
-    void Start () {
-        speed = 0.001f;
-	}
-	
-	void Update () {
-        if (!PhotonNetwork.isMasterClient)
+    void Start()
+    {
+        speed = 0.005f;
+    }
+
+    void Update()
+    {
+        if (!PhotonNetwork.isMasterClient || inner.getRadius() <= 1)
         {
             return;
         }
         float delta = Time.time - startTime;
         float shrinkDelta = Time.time - shrinkTime;
-        if(isShrinked && shrinkDelta >= .1f)
+        if (isShrinked && shrinkDelta >= .1f)
         {
             this.RpcShrink();
             shrinkTime = Time.time;
         }
 
-        if(delta >= 10f)
+        if (delta >= waitTime)
         {
             if (isShrinked)
             {
                 this.RpcfinishShrink();
                 isShrinked = false;
                 this.setCenter();
+                waitTime = 20f;
             }
             else
             {
                 isShrinked = true;
                 this.startShrink();
+                waitTime = 10f;
             }
             startTime = Time.time;
         }
-	}
+    }
     void startShrink()
     {
         Vector2 speed = inner.getCenter() - outer.getCenter();
@@ -55,17 +61,19 @@ public class circleControl : Photon.MonoBehaviour {
     {
         Vector2 outerCenter = outer.getCenter();
         float innerRadius = inner.getRadius();
-        if(innerRadius <= 0.2f)
+        if (innerRadius <= 0.2f)
         {
             return;
         }
-        float newx = Random.Range(-2, 2);
-        float newy = Random.Range(-2, 2);
+        float R = outer.getRadius();
+        float newx = Random.Range(-2 * R, 2 * R);
+        float newy = Random.Range(-2 * R, 2 * R);
         Vector2 innerCenter = outerCenter + new Vector2(newx, newy);
-        innerRadius -= .1f;
+        innerRadius = R * 2 / 3;
+        speed = R / 300;
         this.RpcSetCenter(innerCenter, innerRadius);
     }
-    
+
     void RpcShrink()
     {
         float newRadius = outer.getRadius() - speed;
