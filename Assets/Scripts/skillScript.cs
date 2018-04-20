b61a70798b732d7d657bbccf20edae797be568d3
Jason123123 <641478785@qq.com>
2018-04-20 09:51:59 -0700

    change bomb button image

--- Assets/Scripts/skillScript.cs
+++ Assets/Scripts/skillScript.cs
@@ -9,6 +9,8 @@ public class skillScript : MonoBehaviour {
 	public Button gumBtn;
 	private bool hasBomb = false;
 	private bool hasGum = false;
+	private int bombCounter = 0;
+	private int gumCounter = 0;
 
 	// Use this for initialization
 
@@ -28,28 +30,48 @@ public class skillScript : MonoBehaviour {
 		if (collision.tag == "bomb" && !hasBomb) 
 		{
 			bombBtn.gameObject.SetActive (true);
+			hasBomb = true;
+			bombCounter = 5;
 		} 
 		else if (collision.tag == "chewingGum" && !hasGum) 
 		{
 			gumBtn.gameObject.SetActive (true);
+			hasGum = true;
+			gumCounter = 5;
 		}
 	}
 
 
 	public void throwBomb()
 	{
+		if (bombCounter == 0) 
+		{
+			hasBomb = false;
+			bombBtn.gameObject.SetActive (false);
+			return;
+		}
 		GameObject bomb = PhotonNetwork.Instantiate ("Bomb", transform.position, transform.rotation, 0);
 		Vector3 direction = transform.GetChild (0).transform.position - transform.position;
 		direction /= Vector3.Magnitude (direction);
 		Vector3 startPos = transform.position;
-		Vector3 endPos = transform.position + 10 * direction;
-		bomb.transform.position = endPos;
+		Vector3 endPos = transform.position + direction;
+		bomb.transform.position = startPos;
+
+		bombCounter--;
 	}
 
 	public void throwGum()
 	{
+		if (gumCounter == 0) 
+		{
+			hasGum = false;
+			gumBtn.gameObject.SetActive (false);
+			return;
+		}
 		Vector3 startPos = transform.position;
 		startPos.y += 100;
 		GameObject gum = PhotonNetwork.Instantiate ("ChewingGum", startPos, transform.rotation, 0);
+
+		gumCounter--;
 	}
 }
